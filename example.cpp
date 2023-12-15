fd34910a0391406329f80dd18a78c0bc4a858504
Frida <fdesigley@gmail.com>
2023-12-15 09:59:17 +0000

    some first experiments

---
+++ example.cpp
@@ -0,0 +1,14 @@
+/*
+a hello world program
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"hello world"<<endl;
+    return 1;
+}
+
+
---
+++ example_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// a function that prints a message 10 times
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        std::cout << "hello this is a pleasant cpp pinaple" << std::endl;
+    }
+    return 0;
+}
+
+// a function that prints all the words in a string
+
+// int main() {
+//     std::string str; "hello this is a pleasant cpp pinaple";
+//     for (int i = 0; i < str.length(); i++) {
+//         std::cout << str[i] << std::endl;
+//     }
+//     return 0;
+// }
+
